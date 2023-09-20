505eabde0b8c1980aac99a3ae89fc7d5b03a20d8
app-stack <225businesses@gmail.com>
2023-09-20 11:52:32 +0200

    sauv1_20_09_23

---
+++ mvc/App/Utils/messagerie.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Model;
+class Messagerie(
+    public static function sendEmail(){
+        Messagerie::sendEmail($destinataire, $expediteur, $objet, $contenu){
+            
+        }
+    }
+)
+
+?>
--- mvc/env.php
+++ mvc/env.php
@@ -3,4 +3,8 @@
     $database = 'chocoblast';
     $login= 'root';
     $password = '';
+    $login_messagerie = "";
+    $password_messagerie = "";
+    $serveur_messagerie = "";
+    $port_messagerie = "";
 ?>
